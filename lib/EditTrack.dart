54f1484b95b3b946d91424edfb5856165c59a6f5
tanapolNon <610107030016@dpu.ac.th>
2022-06-02 16:13:14 +0700

    edit

--- lib/EditTrack.dart
+++ lib/EditTrack.dart
@@ -10,9 +10,143 @@ class EditTrack extends StatefulWidget {
 class _EditTrackState extends State<EditTrack> {
   @override
   Widget build(BuildContext context) {
-    return 
-    Scaffold(
+    return Scaffold(
       backgroundColor: Colors.lightBlue[100],
-      body: const SingleChildScrollView());
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(100, 50, 35, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'เเก้ไขข้อมูลอุปกรณ์',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(100, 50, 35, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'สภาพการใช้งาน',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(100, 5, 100, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(100, 50, 35, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'ตำเเหน่งที่ตั้ง',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(100, 5, 100, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(100, 50, 35, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'หมายเหตุ',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(100, 5, 100, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
+                ),
+              ),
+              maxLines: 4,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(800, 0, 0, 0),
+            child: Row(
+              children: [
+                SizedBox(
+                    height: 40,
+                    width: 130,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.orange),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            side: BorderSide(width: 2),
+                          ))),
+                      onPressed: () {},
+                      child: const Text("ยืนยัน",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                          textAlign: TextAlign.center),
+                    )),
+                SizedBox(
+                  width: 30,
+                ),
+                SizedBox(
+                    height: 40,
+                    width: 130,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.redAccent),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            side: BorderSide(width: 2),
+                          ))),
+                      onPressed: () {},
+                      child: const Text("ยกเลิก",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                          textAlign: TextAlign.center),
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
