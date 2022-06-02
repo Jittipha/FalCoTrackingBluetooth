e0a4e206176edba7fe9b0ffbea4c424c4a494f4f
Gunkawee <94429357+Gunkawee@users.noreply.github.com>
2022-06-02 16:01:54 +0700

    update

---
+++ lib/model/track.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Track {
+  String? Track_ID;
+  String? Brand;
+  String? Generation;
+  String? Menufacurer;
+  String? Size;
+  int? Age_of_use;
+  String? Location;
+  String? Work_for;
+  String? Start_Enable_Date;
+  String? Last_Improve_Date;
+  int? Count_Improve;
+  String? End_Date;
+  String? Note;
+
+  Track({
+    this.Track_ID,
+    this.Brand,
+    this.Generation,
+    this.Menufacurer,
+    this.Size,
+    this.Age_of_use,
+    this.Location,
+    this.Work_for,
+    this.Start_Enable_Date,
+    this.Last_Improve_Date,
+    this.Count_Improve,
+    this.End_Date,
+    this.Note,
+  });
+}
