2a5612f4a0b33e51d79340df298ac2ba4342740c
Gunkawee <94429357+Gunkawee@users.noreply.github.com>
2022-06-10 14:12:22 +0700

    repairlist

---
+++ lib/repairlist.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class repairlist extends StatefulWidget {
+  const repairlist({Key? key}) : super(key: key);
+
+  @override
+  State<repairlist> createState() => _repairlistState();
+}
+
+class _repairlistState extends State<repairlist> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 179, 229, 252),
+    );
+  }
+}
