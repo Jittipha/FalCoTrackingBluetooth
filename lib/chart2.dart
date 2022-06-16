768fb9bae11b17f284e996da0a30296430df6a12
tanapolNon <610107030016@dpu.ac.th>
2022-06-16 12:25:29 +0700

    validaterepair

--- lib/chart2.dart
+++ lib/chart2.dart
@@ -29,8 +29,8 @@ class PieChart2State extends State {
                   children: <Widget>[
                     CircularPercentIndicator(
                       radius: 45,
-                      lineWidth: 4.0,
-                      percent: 0.30,
+                      lineWidth: 30.0,
+                      percent: 0.10,
                       center: const Text("10%"),
                       progressColor: Colors.red,
                       animation: true,
@@ -47,7 +47,7 @@ class PieChart2State extends State {
                     //     ),
                      CircularPercentIndicator(
                       radius: 45.0,
-                      lineWidth: 4.0,
+                      lineWidth: 30.0,
                       percent: 0.30,
                       center:  const Text("30%"),
                       progressColor: Colors.orange,
@@ -59,7 +59,7 @@ class PieChart2State extends State {
                     // ),
                     CircularPercentIndicator(
                       radius: 45.0,
-                      lineWidth: 4.0,
+                      lineWidth: 30.0,
                       percent: 0.60,
                       center: const Text("60%"),
                       progressColor: Colors.yellow,
@@ -71,9 +71,9 @@ class PieChart2State extends State {
                     // ),
                     CircularPercentIndicator(
                       radius: 45.0,
-                      lineWidth: 4.0,
-                      percent: 0.90,
-                      center: const Text("90%"),
+                      lineWidth: 30.0,
+                      percent: 0.50,
+                      center: const Text("50%"),
                       progressColor: Colors.green,
                       animation: true,
                       animationDuration: 900,
